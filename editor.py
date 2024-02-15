5764d2b33a3fa1e21a5c98554eed1aeb0ce1bfe7
tombackert <backert.github@gmail.com>
2024-02-15 14:58:38 +0100

    editor window setup

---
+++ editor.py
@@ -0,0 +1,95 @@
+import pygame
+import sys
+from scripts.utils import load_images, load_image
+from scripts.tilemap import Tilemap
+
+RENDER_SCALE = 2.0
+
+class Editor:
+    def __init__(self):
+        
+        pygame.init()
+
+        pygame.display.set_caption('editor')
+        self.screen = pygame.display.set_mode((640, 480))
+        self.display = pygame.Surface((320, 240))
+
+        self.clock = pygame.time.Clock()
+
+        
+
+        self.assets = {
+            'decor': load_images('tiles/decor'),
+            'grass': load_images('tiles/grass'),
+            'large_decor': load_images('tiles/large_decor'),
+            'stone': load_images('tiles/stone'),
+            'background': load_image('background.png')
+        }
+
+        self.movement = [False, False, False, False]
+
+        self.tilemap = Tilemap(self, tile_size=16)
+
+        self.scroll = [0, 0]
+
+
+    def run(self):
+        while True:
+            self.display.blit(self.assets['background'], (0, 0))
+
+
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                
+                # movement keys
+                if event.type == pygame.KEYDOWN:
+                    # w, a, s, d
+                    if event.key == pygame.K_a:
+                        self.movement[0] = True
+                    if event.key == pygame.K_d:
+                        self.movement[1] = True
+                    if event.key == pygame.K_w:
+                        self.movement[2] = True
+                    if event.key == pygame.K_s:
+                        self.movement[3] = True                 
+
+                    # arrow keys                   
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = True
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = True
+                    if event.key == pygame.K_UP:
+                        self.movement[2] = True
+                    if event.key == pygame.K_DOWN:
+                        self.movement[3] = True
+                    
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_a:
+                        self.movement[0] = False
+                    if event.key == pygame.K_d:
+                        self.movement[1] = False
+                    if event.key == pygame.K_w:
+                        self.movement[2] = False
+                    if event.key == pygame.K_s:
+                        self.movement[3] = False
+
+
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = False
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = False
+                    if event.key == pygame.K_UP:
+                        self.movement[2] = False
+                    if event.key == pygame.K_DOWN:
+                        self.movement[3] = False
+                    
+                    
+
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
+            pygame.display.update()
+            self.clock.tick(60) # 60fps
+
+Editor().run()
