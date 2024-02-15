85a97039b1defade2a4c03c57379436058746250
tombackert <backert.github@gmail.com>
2024-02-15 14:42:51 +0100

    introducing animations

--- scripts/utils.py
+++ scripts/utils.py
@@ -13,3 +13,26 @@ def load_images(path):
     for img_name in sorted(os.listdir(BASE_IMG_PATH + path)):
         images.append(load_image(path + '/' + img_name))
     return images
+
+
+class Animation:
+    def __init__(self, images, img_dur=5, loop=True):
+        self.images = images
+        self.loop = loop
+        self.img_duration = img_dur
+        self.done = False
+        self.frame = 0
+
+    def copy(self):
+        return Animation(self.images, self.img_duration, self.loop)
+    
+    def update(self):
+        if self.loop:
+            self.frame = (self.frame + 1) % (len(self.images) * self.img_duration)
+        else:
+            self.frame = min(self.frame + 1, len(self.images) * self.img_duration - 1)
+            if self.frame >= len(self.images) * self.img_duration - 1:
+                self.done = True
+
+    def img(self):
+        return self.images[int(self.frame / self.img_duration)]
