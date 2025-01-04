366a71aae339c8f41a881dffc586c46c837ded00
tombackert <backert.github@gmail.com>
2025-01-04 00:00:03 -0500

    Outsource the game element rendering to UI class

--- game.py
+++ game.py
@@ -198,85 +198,8 @@ class Game:
                 self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
                 render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
 
-                #### Graphics rendering start
-
-                # Leaf particles
-                for rect in self.leaf_spawners:
-                    if random.random() * 49999 < rect.width * rect.height:
-                        pos = (rect.x + random.random() * rect.width, rect.y + random.random() * rect.height)
-                        self.particles.append(Particle(self, 'leaf', pos, velocity=[-0.1, 0.3], frame=random.randint(0, 20)))
-
-                # Clouds
-                self.clouds.update()
-                self.clouds.render(self.display_2, offset=render_scroll)
-                self.tilemap.render(self.display, offset=render_scroll)
-
-                # Enemies
-                for enemy in self.enemies.copy():
-                    kill = enemy.update(self.tilemap, (0, 0))
-                    enemy.render(self.display, offset=render_scroll)
-                    if kill:
-                        self.enemies.remove(enemy)
-
-                if not self.dead:
-                    for player in self.players:
-                        player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
-                        player.render(self.display, offset=render_scroll)
-
-                # Projectiles
-                for projectile in self.projectiles.copy():
-                    projectile[0][0] += projectile[1]
-                    projectile[2] += 1
-                    img = self.assets['projectile']
-                    self.display.blit(img, (projectile[0][0] - img.get_width() / 2 - render_scroll[0], projectile[0][1] - img.get_height() / 2 - render_scroll[1]))
-                    if self.tilemap.solid_check(projectile[0]):
-                        self.projectiles.remove(projectile)
-                        for i in range(4):
-                            self.sparks.append(Spark(projectile[0], random.random() - 0.5 + (math.pi if projectile[1] > 0 else 0), 2 + random.random()))
-                    elif projectile[2] > 360:
-                        self.projectiles.remove(projectile)
-                    elif abs(self.player.dashing) < 50:
-                        if self.player.rect().collidepoint(projectile[0]):
-                            self.projectiles.remove(projectile)
-                            self.player.lifes -= 1
-                            self.sfx['hit'].play()
-                            self.screenshake = max(16, self.screenshake)
-                            for i in range(30):
-                                angle = random.random() * math.pi * 2
-                                speed = random.random() * 5
-                                self.sparks.append(Spark(self.player.rect().center, angle, 2 + random.random()))
-                                self.particles.append(Particle(
-                                    self, 'particle', self.player.rect().center,
-                                    velocity=[math.cos(angle + math.pi) * speed * 0.5, math.sin(angle + math.pi) * speed * 0.5],
-                                    frame=random.randint(0, 7)
-                                ))
-
-                # Sparks
-                for spark in self.sparks.copy():
-                    kill = spark.update()
-                    spark.render(self.display, offset=render_scroll)
-                    if kill:
-                        self.sparks.remove(spark)
-                
-                # Collectables updaten & rendern
-                self.collectable_manager.update(self.player.rect())
-                self.collectable_manager.render(self.display, offset=render_scroll)
-
-                display_mask = pygame.mask.from_surface(self.display)
-                display_sillhouette = display_mask.to_surface(setcolor=(0, 0, 0, 180), unsetcolor=(0, 0, 0, 0))
-                for offset_o in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                    self.display_2.blit(display_sillhouette, offset_o)
-
-                # Particles
-                for particle in self.particles.copy():
-                    kill = particle.update()
-                    particle.render(self.display, offset=render_scroll)
-                    if particle.type == 'leaf':
-                        particle.pos[0] += math.sin(particle.animation.frame * 0.035) * 0.3
-                    if kill:
-                        self.particles.remove(particle)
-
-                #### Graphics rendering end
+                #Graphics rendering
+                UI.render_game_elements(self, render_scroll)
 
                 # Keyboard events
                 self.keyboard_manager.handle_keydown()
