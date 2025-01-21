db0b50408d20222e4770644c81999b87fdd9c4c2
tombackert <backert.github@gmail.com>
2025-01-21 22:21:56 +0100

    Gameplay improvements

--- scripts/entities.py
+++ scripts/entities.py
@@ -8,6 +8,7 @@ from scripts.particle import Particle
 from scripts.spark import Spark
 from scripts.settings import settings
 from scripts.collectableManager import CollectableManager as cm
+import math
 
 
 class PhysicsEntity:
@@ -100,26 +101,31 @@ class Enemy(PhysicsEntity):
                 if (self.collisions['right'] or self.collisions['left']):
                     self.flip = not self.flip
                 else:
-                    movement = (movement[0] - 0.5 if self.flip else 0.5, movement[1])
+                    direction = 0.35 * (1 + 0.8 * math.log(settings.selected_level + 1))
+                    movement = (movement[0] - direction if self.flip else direction, movement[1])
             else:
                 self.flip = not self.flip
             self.walking = max(0, self.walking - 1)
             if not self.walking:
                 dis = (self.game.player.pos[0] - self.pos[0], 
                        self.game.player.pos[1] - self.pos[1])
-                if (abs(dis[1]) < 10):
+                if (abs(dis[1]) < 15):
                     if (self.flip and dis[0] < 0):
                         self.game.sfx['shoot'].play()
-                        self.game.projectiles.append([[self.rect().centerx - 7, 
-                                                       self.rect().centery], -1.5, 0])
+                        direction = -1.15 * (1 + 0.59 * math.log(settings.selected_level + 1))
+                        self.game.projectiles.append([[self.rect().centerx - 15, 
+                                                       self.rect().centery], direction, 0])
                         for i in range(4):
                             self.game.sparks.append(
                                 Spark(self.game.projectiles[-1][0], 
                                       random.random() - 0.5 + math.pi, 
                                       2 + random.random()))
+                        
                     if (not self.flip and dis[0] > 0):
-                        self.game.projectiles.append([[self.rect().centerx + 7, 
-                                                       self.rect().centery], 1.5, 0])
+                        self.game.sfx['shoot'].play()
+                        direction = 1.15 * (1 + 0.59 * math.log(settings.selected_level + 1))
+                        self.game.projectiles.append([[self.rect().centerx + 15, 
+                                                       self.rect().centery], direction, 0])
                         for i in range(4):
                             self.game.sparks.append(
                                 Spark(self.game.projectiles[-1][0], 
@@ -129,7 +135,6 @@ class Enemy(PhysicsEntity):
             self.walking = random.randint(30, 120)
         
         super().update(tilemap, movement=movement)
-        
         if movement[0] != 0:
             self.set_action('run')
         else:
@@ -201,7 +206,7 @@ class Player(PhysicsEntity):
     def shoot(self):
         if self.game.cm.gun and self.game.cm.ammo > 0 and self.shoot_cooldown == 0 and settings.selected_weapon == 1:
             self.game.sfx['shoot'].play()
-            direction = -1.5 if self.flip else 1.5
+            direction = -3.5 if self.flip else 3.5
             self.game.projectiles.append([
                 [self.rect().centerx + (7 * (-1 if self.flip else 1)), 
                  self.rect().centery], 
